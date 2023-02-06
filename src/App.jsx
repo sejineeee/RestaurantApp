9093a31f5cd7f7f52a4d40293c5b7e366a00fa9e
sejineeee <sejinee0918@gmail.com>
2023-02-06 19:19:43 +0900

    feat: App 파일 라우트 연결 및 리셋스타일 적용
    Route 연결
    accessToken이 있을 경우 LocalStorage에 저장되도록 구현
    reset style 연결

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { useDispatch } from 'react-redux';
+
+import { Global } from '@emotion/react';
+
+import Header from './components/Header';
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+import RestaurantsPage from './RestaurantsPage';
+import RestaurantPage from './RestaurantPage';
+import NotFoundPage from './NotFoundPage';
+import LoginPage from './LoginPage';
+
+import { setAccessToken } from './slice';
+
+import { loadItem } from './services/storage';
+
+import reset from './resetStyle';
+
+export default function App() {
+  const dispatch = useDispatch();
+
+  const accessToken = loadItem('accessToken');
+  if (accessToken) {
+    dispatch(setAccessToken(accessToken));
+  }
+  return (
+    <div>
+      <Global styles={reset} />
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="/restaurants/:id" element={<RestaurantPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
+}
