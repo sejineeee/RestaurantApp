3f64a05f665e2398d8db87bc92ea72fde2ef818e
sejineeee <sejinee0918@gmail.com>
2023-02-08 23:18:14 +0900

    slide에 사용할 이미지 배열에 할당

---
+++ src/slider-img.js
@@ -0,0 +1,10 @@
+const sliderImg = [
+  { id: 1, src: '/img/slider-img-1.jpg', alt: '과일 사진' },
+  { id: 2, src: '/img/slider-img-2.jpg', alt: '피자 사진' },
+  { id: 3, src: '/img/slider-img-3.jpg', alt: '햄버거 사진' },
+  { id: 4, src: '/img/slider-img-4.jpg', alt: '스테이크 사진' },
+  { id: 5, src: '/img/slider-img-5.jpg', alt: '일본 라면 사진' },
+  { id: 6, src: '/img/slider-img-6.jpg', alt: '비빔밥 사진' },
+];
+
+export default sliderImg;
