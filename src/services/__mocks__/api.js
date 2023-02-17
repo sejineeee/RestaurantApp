549c2d7482c0451bd181bfdca0f49aa73e3252e8
sejineeee <sejinee0918@gmail.com>
2023-02-17 21:50:31 +0900

    test: mocks 생성

---
+++ src/services/__mocks__/api.js
@@ -0,0 +1,35 @@
+export async function fetchRegions() {
+  return [];
+}
+
+export async function fetchCategories() {
+  return [];
+}
+
+export async function fetchRestaurants() {
+  return [];
+}
+
+export async function fetchRestaurant(restaurantId) {
+  return {
+    id: restaurantId,
+  };
+}
+
+export async function postLogin({ email, password }) {
+  return {
+    email,
+    password,
+  };
+}
+
+export async function postReview({
+  accessToken, restaurantId, score, description,
+}) {
+  return {
+    accessToken,
+    restaurantId,
+    score,
+    description,
+  };
+}
