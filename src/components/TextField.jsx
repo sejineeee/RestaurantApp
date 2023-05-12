d71bd94d414348ca157cd5e9912ca185b0e3cf98
sejineeee <sejinee0918@gmail.com>
2023-05-12 18:01:12 +0900

    feat: TextField 컴포넌트 구현

---
+++ src/components/TextField.jsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div({
+  '& label': {
+    fontSize: '2.4rem',
+    fontWeight: 'bold',
+    lineHeight: '1.3',
+    margin: '8px 0',
+  },
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const Field = styled.input(({ name }) => ({
+  width: name === 'score' ? '180px' : '100%',
+  height: name === 'score' ? '34px' : '200px',
+  border: '1px solid #D9D9D9',
+  borderRadius: '5px',
+  padding: '8px 16px',
+  boxSizing: 'border-box',
+}));
+
+export default function TextField({
+  label,
+  type = 'text',
+  name,
+  value,
+  onChange,
+}) {
+  const id = `input-${name}`;
+
+  function handleChange(event) {
+    const { target } = event;
+    onChange({ name, value: target.value });
+  }
+  return (
+    <Container>
+      <label htmlFor={id}>{label}</label>
+      <Field
+        type={type}
+        id={id}
+        name={name}
+        value={value}
+        onChange={handleChange}
+      />
+    </Container>
+  );
+}
