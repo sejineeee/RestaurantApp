66813d0c70c1ddb7d3d3a70af1a1b0e8ced18666
sejineeee <sejinee0918@gmail.com>
2023-05-12 15:08:18 +0900

    feat: region 컴포넌트 구현

---
+++ src/pages/RestaurantsPage/Regions.jsx
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled';
+
+import MenuList from '../../assets/emotion/MenuList';
+import MenuItem from '../../assets/emotion/MenuItem';
+
+import { regionsImg } from '../../assets/selectionImg';
+
+const Title = styled.div({
+  fontFamily: 'Noto Sans KR, sans-serif',
+  fontSize: '2.4rem',
+  fontWeight: 'bold',
+});
+
+const ImgWrap = styled.div({
+  border: '1px solid #C2C2C2',
+  cursor: 'pointer',
+});
+
+export default function Regions({
+  regions,
+  onClick,
+  selectedRegion,
+}) {
+  const MenuItemImage = ({ region, onClick }) => regionsImg.map((regionImg) => (
+    regionImg.alt === region.name
+      ? (
+        <ImgWrap onClick={() => onClick(region.id)}>
+          <img src={regionImg.src} alt={regionImg.alt} />
+        </ImgWrap>
+      )
+      : null
+  ));
+
+  return (
+    <>
+      <Title>지역</Title>
+      <MenuList>
+        {
+          regions.map((region) => (
+            <MenuItem
+              key={region.id}
+              active={selectedRegion && region.id === selectedRegion.id}
+            >
+              <MenuItemImage region={region} onClick={onClick} />
+              <button
+                key={region.id}
+                type="button"
+                onClick={() => onClick(region.id)}
+              >
+                {region.name}
+                {
+                selectedRegion
+                && region.id === selectedRegion.id
+                  ? '✅'
+                  : null
+                }
+              </button>
+            </MenuItem>
+          ))
+        }
+      </MenuList>
+    </>
+  );
+}
