dcdd490d6fd8aac7699370762ffca12874c3af14
sejineeee <sejinee0918@gmail.com>
2023-05-12 15:35:32 +0900

    feat: restaurant 상세 페이지 구현

---
+++ src/pages/RestaurantPage/RestaurantPage.jsx
@@ -0,0 +1,22 @@
+import { useParams } from 'react-router-dom';
+
+import styled from '@emotion/styled';
+
+import RestaurantContainer from './RestaurantContainer';
+
+const Container = styled.div({
+  width: '90%',
+  margin: '0 auto',
+  padding: '32px 24px',
+
+});
+
+export default function RestaurantPage({ params }) {
+  const { id } = params || useParams();
+
+  return (
+    <Container>
+      <RestaurantContainer restaurantId={id} />
+    </Container>
+  );
+}
