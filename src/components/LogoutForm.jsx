fd5c15208ba534e131071b800a3b0b640ed4b566
sejineeee <sejinee0918@gmail.com>
2023-05-13 00:58:45 +0900

    feat: logout form 구현

---
+++ src/components/LogoutForm.jsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button({
+  backgroundColor: 'transparent',
+  border: 'none',
+  lineHeight: 0,
+  letterSpacing: '.2rem',
+  fontSize: '1.8rem',
+  fontFamily: 'Roboto, sans-serif',
+  fontWeight: 'bold',
+  color: '#555555',
+  cursor: 'pointer',
+  '&:hover': {
+    color: '#D78521',
+  },
+});
+
+export default function LogoutForm({ onClick }) {
+  return (
+    <Button type="button" onClick={onClick}>
+      Log out
+    </Button>
+  );
+}
