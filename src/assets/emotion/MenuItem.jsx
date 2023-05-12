d7d67bd6844bfba4dce2ddbd07488f3201f18b63
sejineeee <sejinee0918@gmail.com>
2023-05-12 18:02:03 +0900

    feat: menuItem style

---
+++ src/assets/emotion/MenuItem.jsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+
+const MenuItem = styled.li(({ active }) => ({
+  marginRight: '2rem',
+  marginBottom: '1.6rem',
+  fontSize: '1.6rem',
+  fontFamily: 'Noto Sans KR, sans-serif',
+  textAlign: 'center',
+
+  '& div': {
+    width: '124px',
+    height: '124px',
+    borderRadius: '50%',
+    overflow: 'hidden',
+    transition: 'all 1s',
+  },
+
+  '& img': {
+    width: '124px',
+    height: '124px',
+    overflow: 'hidden',
+    borderRadius: '50%',
+    display: 'block',
+  },
+
+  '& button': {
+    fontSize: '1.6rem',
+    fontFamily: 'Noto Sans KR, sans-serif',
+    border: 0,
+    cursor: 'pointer',
+    textDecoration: 'none',
+    color: '#000000',
+    fontWeight: 'bold',
+  },
+}));
+
+export default MenuItem;
