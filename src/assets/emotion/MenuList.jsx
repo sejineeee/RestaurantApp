7079cb57dbaf3d4eaf722890bedc71e3089521bc
sejineeee <sejinee0918@gmail.com>
2023-05-12 18:02:17 +0900

    feat: menuList style

---
+++ src/assets/emotion/MenuList.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const MenuList = styled.ul({
+  display: 'flex',
+  flexWrap: 'wrap',
+  margin: 0,
+  padding: '1rem 0',
+  listStyle: 'none',
+  fontFamily: 'Noto Sans KR, sans-serif',
+  fontSize: '2.4rem',
+  fontWeight: 'bold',
+});
+
+export default MenuList;
