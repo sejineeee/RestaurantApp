65a5650a39d07fc59e06d7ad9979174b7b1ae94f
sejineeee <sejinee0918@gmail.com>
2023-06-27 18:03:42 +0900

    chore: output 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist'
+  },
   module: {
     rules: [
       {
