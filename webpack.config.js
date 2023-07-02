6c6b71226364d4ed270f8f89db5bd9b71187d45d
sejineeee <sejinee0918@gmail.com>
2023-07-02 22:43:55 +0900

    error로 인해 output 제거

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,6 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname + '/dist'),
-    clean: true,
-  },
   module: {
     rules: [
       {
