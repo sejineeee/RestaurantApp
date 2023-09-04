f75afbd706caa15971f3a269008751d1781342de
sejineeee <sejinee0918@gmail.com>
2023-09-04 14:46:02 +0900

    Fix: pagination buttons skipByTen 메서드 호출시 setPage 호출 추가
    skipByTen 버튼 클릭시 page 연동 안되는 에러
    해당 메서드 내 setPage에 page에서 10을 추가하고 더해줌으로써 수정

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
-import { RxDoubleArrowLeft, RxDoubleArrowRight } from 'react-icons/rx';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { RxDoubleArrowLeft, RxDoubleArrowRight } from "react-icons/rx";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const Container = styled.div({
-  padding: '16px 0',
-  display: 'flex',
-  justifyContent: 'center',
+  padding: "16px 0",
+  display: "flex",
+  justifyContent: "center",
 });
 
 const Button = styled.button(({ active }) => ({
-  backgroundColor: active ? '#FFBD60' : 'transparent',
-  color: active ? '#ffffff' : '#000000',
-  width: '40px',
-  height: '40px',
-  fontSize: '1.6rem',
-  fontFamily: 'Noto Sans KR, sans-serif',
+  backgroundColor: active ? "#FFBD60" : "transparent",
+  color: active ? "#ffffff" : "#000000",
+  width: "40px",
+  height: "40px",
+  fontSize: "1.6rem",
+  fontFamily: "Noto Sans KR, sans-serif",
 }));
 
-function Pagination({
-  totalCount,
-  limit,
-  page,
-  setPage,
-}) {
+function Pagination({ totalCount, limit, page, setPage }) {
   const [paginationBarCount, setPaginationBarCount] = useState(1);
 
   const totalPages = Math.ceil(totalCount / limit);
@@ -39,12 +34,14 @@ function Pagination({
         return null;
       }
       setPaginationBarCount(paginationBarCount - 1);
+      setPage(page - 10);
     },
     nextSkipByTen() {
       if (paginationBarCount === Math.ceil(totalPages / limit)) {
         return null;
       }
       setPaginationBarCount(paginationBarCount + 1);
+      setPage(page + 10);
     },
     prev() {
       if (page === 1) {
@@ -76,43 +73,29 @@ function Pagination({
 
   return (
     <Container>
-      <Button
-        type="button"
-        onClick={() => handleClick('prevSkipByTen')}
-      >
+      <Button type="button" onClick={() => handleClick("prevSkipByTen")}>
         <RxDoubleArrowLeft />
       </Button>
-      <Button
-        type="button"
-        onClick={() => handleClick('prev')}
-      >
+      <Button type="button" onClick={() => handleClick("prev")}>
         <IoIosArrowBack />
       </Button>
-      {
-        Array(totalPages)
-          .fill()
-          .slice(offset, offset + limit)
-          .map((_, i) => (
-            <Button
-              key={i + 1}
-              type="button"
-              onClick={() => handleClick(_, offset + (i + 1))}
-              active={page === (offset + (i + 1))}
-            >
-              {offset + (i + 1)}
-            </Button>
-          ))
-      }
-      <Button
-        type="button"
-        onClick={() => handleClick('next')}
-      >
+      {Array(totalPages)
+        .fill()
+        .slice(offset, offset + limit)
+        .map((_, i) => (
+          <Button
+            key={i + 1}
+            type="button"
+            onClick={() => handleClick(_, offset + (i + 1))}
+            active={page === offset + (i + 1)}
+          >
+            {offset + (i + 1)}
+          </Button>
+        ))}
+      <Button type="button" onClick={() => handleClick("next")}>
         <IoIosArrowForward />
       </Button>
-      <Button
-        type="button"
-        onClick={() => handleClick('nextSkipByTen')}
-      >
+      <Button type="button" onClick={() => handleClick("nextSkipByTen")}>
         <RxDoubleArrowRight />
       </Button>
     </Container>
@@ -126,4 +109,4 @@ Pagination.propTypes = {
   limit: PropTypes.number,
   page: PropTypes.number,
   setPage: PropTypes.func,
-}
+};
